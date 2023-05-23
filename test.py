e72f3b8fb2cf61a813b9d896a61457e0882738cf
Vadim Shcherbakov <pgva@bk.ru>
2023-05-23 17:12:51 +0300

    first_commit_change_bridge_worker

--- test.py
+++
@@ -1,6 +0,0 @@
-from collections import namedtuple
-
-subject_set = {'Physics', 'Chemistry', 'Math', 'English'}
-Marks = namedtuple('Marks', subject_set)
-marks = Marks(90, 85, 95, 100)
-print(marks)
